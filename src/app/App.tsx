6faa365c8a5cce6b0757f03e286d412e63ccc616
kibeshev87 <mrjimb5@gmail.com>
2022-08-27 18:45:43 +0300

    thursday4Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {Login} from "../features/Login/Login";
-import {Route, Routes } from 'react-router-dom'
+import {Route, Routes, Navigate} from 'react-router-dom'
 
 type PropsType = {
     demo?: boolean
@@ -39,9 +39,10 @@ function App({demo = false}: PropsType) {
             </AppBar>
             <Container fixed>
                 <Routes>
-                    <Route path='/' element={<TodolistsList demo={demo}/>}/>
+                    <Route path='/' element={<TodolistsList/>}/>
                     <Route path='login' element={<Login/>}/>
-                    <Route path='*' element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path='404' element={<h1 style={{textAlign: "center"}}>УПС: ЧТО-ТО ПОШЛО НЕ ТАК</h1>}/>
+                    <Route path='*' element={<Navigate to='404'/>}/>
                 </Routes>
             </Container>
         </div>
